e68069e3ced7b962e5609015118c8a106908f4be
李胜锐 <462114286@qq.com>
2020-09-06 21:49:04 +0800

    add all applies info and user

--- core/models.py
+++ core/models.py
@@ -3,10 +3,29 @@ from django.db import models
 # Create your models here.
 
 
-class Costumer(models.Model):
-    pass
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    is_provider = models.BooleanField()
 
 
-class Provider(Costumer):
-    pass
+class RentApply(models.Model):
+    borrower = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
+    target_equipment = models.ForeignKey('Equipment', on_delete=models.SET_NULL)  # 租借设备如果被删则设为空
+    end_time = models.DateTimeField()  # 结束时间
+    reason = models.TextField(max_length=200)
+    state = models.IntegerField()  # 0为等待审核，1为审核通过，2及其余为审核拒绝
+
+
+class OnShelfApply(models.Model):
+    lender = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
+    target_equipment = models.ForeignKey('Equipment', on_delete=models.SET_NULL)  # 租借设备如果被删则设为空
+    remarks = models.TextField(max_length=200)
+    state = models.IntegerField()  # 0为等待审核，1为审核通过，2及其余为审核拒绝
+
+
+class BeingProviderApply(models.Model):
+    applicant = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
+    lab_info = models.TextField(max_length=200)
+    state = models.IntegerField()  # 0为等待审核，1为审核通过，2及其余为审核拒绝
 
