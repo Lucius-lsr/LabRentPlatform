77cf7a7b37d456e56fdcddde1104201e21e4ba7f
blueice12138 <838343608@qq.com>
2020-09-07 00:58:48 +0800

    Add Equipment into models.py

--- core/models.py
+++ core/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-# Create your models here.
-
-
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    phone = models.PhoneNumberField()
+    email = models.EmailField(max_length=254)
     is_provider = models.BooleanField()
 
 
 class BorrowApply(models.Model):
     borrower = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
+    count = models.IntegerField()  # new: borrow number
     target_equipment = models.ForeignKey('Equipment', on_delete=models.SET_NULL)  # 租借设备如果被删则设为空
     end_time = models.DateTimeField()  # 结束时间
     reason = models.TextField(max_length=200)
@@ -19,6 +19,7 @@ class BorrowApply(models.Model):
 
 class OnShelfApply(models.Model):
     lender = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
+    count = models.IntegerField()  # new: count of equipments
     target_equipment = models.ForeignKey('Equipment', on_delete=models.SET_NULL)  # 租借设备如果被删则设为空
     remarks = models.TextField(max_length=200)
     state = models.IntegerField()  # 0为等待审核，1为审核通过，2及其余为审核拒绝
@@ -29,3 +30,11 @@ class UpgradeApply(models.Model):
     lab_info = models.TextField(max_length=200)
     state = models.IntegerField()  # 0为等待审核，1为审核通过，2及其余为审核拒绝
 
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    count = models.IntegerField()
+
+    user = models.ForeignKey("User", related_name='equipments')
+    provider = models.ForeignKey("Provider", on_delete=models.CASCADE, related_name='equipments')
