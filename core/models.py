d0d295dcfe4fa634ec5b122b3149f425ed1ca377
李胜锐 <462114286@qq.com>
2020-09-07 09:43:02 +0800

    modeify gitignore

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     username = models.CharField(max_length=100)  # 只能是学号
     email = models.EmailField()
     password = models.CharField(max_length=100)
-    phone = models.PhoneNumberField()
+    # phone = models.PhoneNumberField()
     is_verified = models.BooleanField(default=False, verbose_name="是否激活")
     is_provider = models.BooleanField(default=False, verbose_name="是否可以提供设备")
 
@@ -22,7 +22,7 @@ class EmailVerifyCode(models.Model):
     code = models.CharField(max_length=20, verbose_name="邮箱验证码")
     email = models.EmailField(max_length=200, verbose_name="验证码邮箱")
     send_type = models.IntegerField(choices=((1, 'register'), (2, 'forget'), (3, 'change')), verbose_name="验证码类型")
-    add_time = models.DateTimeField(default=datetime.now(), verbose_name="添加时间")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     def __str__(self):
         return self.code
@@ -34,8 +34,8 @@ class EmailVerifyCode(models.Model):
 
 class BorrowApply(models.Model):
     borrower = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
-    count = models.IntegerField()  # new: borrow number
-    target_equipment = models.ForeignKey('Equipment', on_delete=models.SET_NULL)  # 租借设备如果被删则设为空
+    count = models.IntegerField(default=0)  # new: borrow number
+    target_equipment = models.ForeignKey('Equipment', on_delete=models.CASCADE)  # 租借设备如果被删则设为空
     end_time = models.DateTimeField()  # 结束时间
     reason = models.TextField(max_length=200)
     state = models.IntegerField(choices=((0, 'pending'), (1, 'accept'), (2, 'refuse')), verbose_name='申请状态')
@@ -50,8 +50,8 @@ class BorrowApply(models.Model):
 
 class OnShelfApply(models.Model):
     lender = models.ForeignKey('User', on_delete=models.CASCADE)  # 申请人如果被删则删除申请
-    count = models.IntegerField()  # new: count of equipments
-    target_equipment = models.ForeignKey('Equipment', on_delete=models.SET_NULL)  # 租借设备如果被删则设为空
+    count = models.IntegerField(default=0)  # new: count of equipments
+    target_equipment = models.ForeignKey('Equipment', on_delete=models.CASCADE)  # 租借设备如果被删则设为空
     remarks = models.TextField(max_length=200)
     state = models.IntegerField(choices=((0, 'pending'), (1, 'accept'), (2, 'refuse')), verbose_name='申请状态')
 
@@ -79,7 +79,7 @@ class UpgradeApply(models.Model):
 class Equipment(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    count = models.IntegerField()
+    count = models.IntegerField(default=0)
 
     provider = models.ForeignKey("User", on_delete=models.CASCADE, related_name='equipments')
 
