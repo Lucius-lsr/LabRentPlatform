c4b91c9b7aeff432dc394b979c47423bf0222859
李胜锐 <462114286@qq.com>
2020-09-09 20:43:56 +0800

    modified admin

--- core/admin.py
+++ core/admin.py
@@ -4,21 +4,27 @@ from core.models import *
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'is_verified', 'is_provider')
+    list_display_links = list_display
     search_fields = ('username', 'email')
 
 
 class EquipmentAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'count', 'provider')
+    list_display_links = list_display
     search_fields = ['name']
+    list_filter = ("onshelfapply__state",)
 
 
 class BorrowApplyAdmin(admin.ModelAdmin):
     list_display = ('id', 'borrower', 'target_equipment', 'count', 'owner', 'end_time', 'state')
+    list_display_links = list_display
     date_hierarchy = 'end_time'
 
 
 class OnShelfApplyAdmin(admin.ModelAdmin):
     list_display = ('id', 'target_equipment', 'state')
+    list_display_links = list_display
+    list_filter = ("state",)
 
 
 # Register your models here.
