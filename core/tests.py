7bc878f252050cf56ef35c2ea1b7c8310c305e46
blueice12138 <838343608@qq.com>
2020-09-10 22:34:01 +0800

    Add more test

--- core/tests.py
+++ core/tests.py
@@ -4,7 +4,7 @@ import json
 
 
 # Create your tests here.
-class UserMethodTest(TestCase):
+class AllMethodTest(TestCase):
     def setUp(self):
         # password = blueice
         User.objects.create(
@@ -33,32 +33,45 @@ class UserMethodTest(TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_login(self):
-        user = User.objects.get(username='user_v1')
-        self.assertIsNotNone(user)
+    def test_login_v1(self):  # user_v1
+        try:
+            User.objects.get(username='user_v1')
+        except:
+            self.assertIsNotNone("user_v1 exist")
+
         response = self.client.patch(
             '/api/v1/login',
             "username=user_v1&password=blueice"
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_logout(self):
+    def test_login_v2(self):  # user_v2
+        try:
+            User.objects.get(username='user_v2')
+        except:
+            self.assertIsNotNone("user_v2 exist")
+
         response = self.client.patch(
             '/api/v1/login',
-            "username=user_v1&password=blueice"
+            "username=user_v2&password=blueice"
         )
         self.assertEqual(response.status_code, 200)
+
+    def logout(self):
+        response = self.client.patch(
+            '/api/v1/logout'
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        self.test_login_v2()
         response = self.client.patch(
             '/api/v1/logout'
         )
         self.assertEqual(response.status_code, 200)
 
     def test_search_equipment(self):
-        response = self.client.patch(
-            '/api/v1/login',
-            "username=user_v1&password=blueice"
-        )
-        self.assertEqual(response.status_code, 200)
+        self.test_login_v2()
         response = self.client.get(
             '/api/v1/search',
             {
@@ -69,27 +82,68 @@ class UserMethodTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_upgrade_apply(self):
-        response = self.client.patch(
-            '/api/v1/login',
-            "username=user_v1&password=blueice"
-        )
-        self.assertEqual(response.status_code, 200)
+        self.test_login_v1()
         response = self.client.put(
             '/api/v1/upgrade',
             "lab_info=This is an upgrade apply"
         )
         self.assertEqual(response.status_code, 200)
-        apply = UpgradeApply.objects.filter(applicant__username='user_v1')
-        self.assertNotEqual(len(apply), 0)
-        apply = apply[0]
-        self.assertEqual(apply.state, 0)
-        apply.state = 1
-        apply.save()
+        try:
+            apply = UpgradeApply.objects.get(applicant__username='user_v1')
+            apply.state = 1
+            apply.save()
+        except:
+            self.assertSequenceEqual("Upgrade success")
 
     def test_on_shelf(self):
-        response = self.client.patch(
-            '/api/v2/login',
-            "username=user_v1&password=blueice"
+        self.test_login_v2()
+        response = self.client.post(
+            '/api/v2/onshelf',
+            {
+                "name": "THU",
+                "description": "THU description",
+                "remarks": "THU remarks",
+                "count": 10
+            }
         )
         self.assertEqual(response.status_code, 200)
+        try:
+            apply = OnShelfApply.objects.get(target_equipment__name="THU")
+            apply.state = 1
+            apply.save()
+        except:
+            self.assertEqual(0, 1)
 
+    def test_off_shelf(self):
+        self.test_on_shelf()
+        try:
+            THU_id = Equipment.objects.get(name="THU").id
+            response = self.client.delete(
+                '/api/v2/offshelf',
+                "equipment_id={}".format(THU_id)
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(Equipment.objects.filter(name="THU")), 0)
+        except:
+            self.assertIsNotNone("THU exist")
+
+    def test_borrow_apply(self):
+        self.test_on_shelf()
+        self.logout()
+        self.test_login_v1()
+        try:
+            THU_id = Equipment.objects.get(name="THU").id
+            response = self.client.post(
+                '/api/v1/apply',
+                {
+                    "id": THU_id,
+                    "endtime": "2020-09-07T15:00:00Z",
+                    "reason": "This is reason for borrow THU",
+                    "count": 3
+                }
+            )
+            self.assertEqual(response.status_code, 200)
+            borrow_apply = BorrowApply.objects.get(target_equipment__name="THU")
+            self.assertIsNotNone(borrow_apply)
+        except:
+            self.assertIsNotNone("Borrow THU success")
